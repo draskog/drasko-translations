20578d546c4aebf39f772f5180cada13623827c1
Drasko Gajic <draskog@gmail.com>
2024-12-11 00:44:59 +0100

    Modify package

--- src/Models/Contributor.php
+++ src/Models/Contributor.php
@@ -3,20 +3,15 @@
 namespace Outhebox\TranslationsUI\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use App\Models\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Outhebox\TranslationsUI\Enums\RoleEnum;
 use Outhebox\TranslationsUI\Traits\HasDatabaseConnection;
 
 class Contributor extends Authenticatable
 {
-    use HasDatabaseConnection;
-    use HasFactory;
-    use Notifiable;
 
     protected $guarded = [];
-
-    protected $table = 'ltu_contributors';
 
     protected $hidden = [
         'password',
@@ -29,12 +24,12 @@ class Contributor extends Authenticatable
 
     public function isOwner(): bool
     {
-        return $this->role === RoleEnum::owner;
+        return true;
     }
 
     public function isTranslator(): bool
     {
-        return $this->role === RoleEnum::translator;
+        return false;
     }
 
     public function isAdministrator(): bool
